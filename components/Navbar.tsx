52ea0a460a086c1bef0e1cb55d93c733d37c5b6d
Brecht Horn <brecht.horn@mac.com>
2024-07-12 19:41:57 -0700

    changes navBar login text color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
-import { AvatarIcon } from "@radix-ui/react-icons";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { AvatarIcon } from '@radix-ui/react-icons';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,16 +8,16 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import Link from "next/link";
-import { Button } from "./ui/button";
-import React from "react";
-import { Database } from "@/types/supabase";
-import ClientSideCredits from "./realtime/ClientSideCredits";
+} from './ui/dropdown-menu';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import React from 'react';
+import { Database } from '@/types/supabase';
+import ClientSideCredits from './realtime/ClientSideCredits';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
-const stripeIsConfigured = process.env.NEXT_PUBLIC_STRIPE_IS_ENABLED === "true";
+const stripeIsConfigured = process.env.NEXT_PUBLIC_STRIPE_IS_ENABLED === 'true';
 
 export const revalidate = 0;
 
@@ -28,53 +28,63 @@ export default async function Navbar() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  const {
-    data: credits,
-  } = await supabase.from("credits").select("*").eq("user_id", user?.id ?? '').single()
+  const { data: credits } = await supabase
+    .from('credits')
+    .select('*')
+    .eq('user_id', user?.id ?? '')
+    .single();
 
   return (
-    <div className="flex w-full px-4 bg-[#131324] lg:px-40 py-4 items-center border-b text-center gap-8 justify-between">
-      <div className="flex gap-2 h-full">
-        <Link href="/">
-          <h2 className="font-bold text-sky-400/100">h<b><u>AI</u></b>dshot Generator</h2>
+    <div className='flex w-full px-4 bg-[#131324] lg:px-40 py-4 items-center border-b text-center gap-8 justify-between'>
+      <div className='flex gap-2 h-full'>
+        <Link href='/'>
+          <h2 className='font-bold text-sky-400/100'>
+            h
+            <b>
+              <u>AI</u>
+            </b>
+            dshot Generator
+          </h2>
         </Link>
       </div>
       {user && (
-        <div className="hidden lg:flex flex-row gap-2">
-          <Link href="/overview">
-            <Button variant={"ghost"}>Home</Button>
+        <div className='hidden lg:flex flex-row gap-2'>
+          <Link href='/overview'>
+            <Button variant={'ghost'}>Home</Button>
           </Link>
           {stripeIsConfigured && (
-            <Link href="/get-credits">
-              <Button variant={"ghost"}>Get Credits</Button>
+            <Link href='/get-credits'>
+              <Button variant={'ghost'}>Get Credits</Button>
             </Link>
           )}
         </div>
       )}
-      <div className="flex gap-4 lg:ml-auto">
+      <div className='flex gap-4 lg:ml-auto text-sky-400/100'>
         {!user && (
-          <Link href="/login">
-            <Button variant={"ghost"}>Login / Signup</Button>
+          <Link href='/login'>
+            <Button variant={'ghost'}>Login / Signup</Button>
           </Link>
         )}
         {user && (
-          <div className="flex flex-row gap-4 text-center align-middle justify-center">
+          <div className='flex flex-row gap-4 text-center align-middle justify-center'>
             {stripeIsConfigured && (
               <ClientSideCredits creditsRow={credits ? credits : null} />
             )}
             <DropdownMenu>
-              <DropdownMenuTrigger asChild className="cursor-pointer">
-                <AvatarIcon height={24} width={24} className="text-primary" />
+              <DropdownMenuTrigger asChild className='cursor-pointer'>
+                <AvatarIcon height={24} width={24} className='text-primary' />
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel className="text-primary text-center overflow-hidden text-ellipsis">{user.email}</DropdownMenuLabel>
+              <DropdownMenuContent className='w-56'>
+                <DropdownMenuLabel className='text-primary text-center overflow-hidden text-ellipsis'>
+                  {user.email}
+                </DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <form action="/auth/sign-out" method="post">
+                <form action='/auth/sign-out' method='post'>
                   <Button
-                    type="submit"
-                    className="w-full text-left"
-                    variant={"ghost"}
-                    >
+                    type='submit'
+                    className='w-full text-left'
+                    variant={'ghost'}
+                  >
                     Log out
                   </Button>
                 </form>
