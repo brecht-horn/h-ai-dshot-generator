075d6a5be032b971952eb6d518eab7284d605622
Brecht Horn <brecht.horn@mac.com>
2024-07-12 19:51:37 -0700

    changes layout bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import { Toaster } from "@/components/ui/toaster";
-import "./globals.css";
-import { Suspense } from "react";
-import { Analytics } from "@vercel/analytics/react";
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import { Toaster } from '@/components/ui/toaster';
+import './globals.css';
+import { Suspense } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: "Headshots AI",
-  description: "Generate awesome headshots in minutes using AI",
+  title: 'Headshots AI',
+  description: 'Generate awesome headshots in minutes using AI',
 };
 
 export default function RootLayout({ children }: any) {
   return (
-    <html lang="en">
-      <body className="min-h-screen flex flex-col">
+    <html lang='en'>
+      <body className='min-h-screen flex flex-col bg-[#222246]'>
         <section>
           <Suspense
             fallback={
-              <div className="flex w-full px-4 lg:px-40 bg-[#222246] py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />
+              <div className='flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]' />
             }
           >
             <Navbar />
           </Suspense>
         </section>
-        <main className="flex flex-1 flex-col items-center py-16">
+        <main className='flex flex-1 flex-col items-center py-16'>
           {children}
         </main>
         <Footer />
