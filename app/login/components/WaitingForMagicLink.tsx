7cfe771014097dc46504a083b4d688420f7466e1
Brecht Horn <brecht.horn@mac.com>
2024-07-13 19:27:52 -0700

    updates waitingformagiclink

--- app/login/components/WaitingForMagicLink.tsx
+++ app/login/components/WaitingForMagicLink.tsx
@@ -9,7 +9,7 @@ export const WaitingForMagicLink = ({
   return (
     <>
       <div className='flex items-center justify-center p-8'>
-        <div className='flex flex-col gap-4 bg-[#131324] dark:bg-[#131324] border border-neutral-200 p-4 rounded-xl max-w-sm w-full'>
+        <div className='flex flex-col gap-4 bg-[#131324] dark:bg-[#131324] p-4 rounded-xl max-w-sm w-full'>
           <h1 className='text-xl text-violet-400'>
             Check your email to continue
           </h1>
