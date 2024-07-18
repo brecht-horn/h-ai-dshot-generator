33649bc410bccab8ad1c56d630bd320e0f2ba48a
Brecht Horn <brecht.horn@mac.com>
2024-07-18 16:06:06 -0700

    updates negative prompts

--- app/overview/models/train/page.tsx
+++ app/overview/models/train/page.tsx
@@ -1,25 +1,25 @@
-import TrainModelZone from "@/components/TrainModelZone";
-import { Button } from "@/components/ui/button";
+import TrainModelZone from '@/components/TrainModelZone';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
+} from '@/components/ui/card';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
 export default async function Index() {
   return (
-    <div className="w-full max-w-2xl mx-auto">
+    <div className='w-full max-w-2xl mx-auto'>
       <div
-        id="train-model-container"
-        className="flex flex-1 flex-col gap-2 px-2"
+        id='train-model-container'
+        className='flex flex-1 flex-col gap-2 px-2'
       >
-        <Link href="/overview" className="text-sm w-fit">
-          <Button variant={"outline"}>
-            <FaArrowLeft className="mr-2 text-white" />
+        <Link href='/overview' className='text-sm w-fit text-white'>
+          <Button variant={'outline'}>
+            <FaArrowLeft className='mr-2 text-white' />
             Go Back
           </Button>
         </Link>
@@ -30,7 +30,7 @@ export default async function Index() {
               Choose a name, type, and upload some photos to get started.
             </CardDescription>
           </CardHeader>
-          <CardContent className="grid gap-6">
+          <CardContent className='grid gap-6'>
             <TrainModelZone />
           </CardContent>
         </Card>
