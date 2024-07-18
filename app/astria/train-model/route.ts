e608772271574c27f3f37994d32f9ac1535b011b
Brecht Horn <brecht.horn@mac.com>
2024-07-18 16:49:19 -0700

    updates prompts

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -139,7 +139,7 @@ export async function POST(request: Request) {
         callback: trainWenhookWithParams,
         prompts_attributes: [
           {
-            text: `face like characters from the Toy Story movies, ohwx ${type} wearing a business suit,only one person, photo accurate hair color, photo accurate eyebrow color, professional photo, animated office background with skyscrapers outside window, disney style, dreamworks style`,
+            text: `face like a character from the Toy Story movies, ohwx ${type} wearing a business suit,only one person, photo accurate hair color, photo accurate eyebrow color, professional photo, animated office background with skyscrapers outside window, disney style, dreamworks style`,
             // text: `pixar style animated portrait of ohwx ${type} wearing a business suit, face like characters from the Toy Story movies, only one person, photo accurate hair color, photo accurate eyebrow color, professional photo, animated office background with skyscrapers outside window, disney style, dreamworks style`,
             negative_prompt:
               'painting, extra fingers, mutated hands, poorly drawn hands, ((poorly drawn face,)) deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime, nude, NSFW',
@@ -147,7 +147,7 @@ export async function POST(request: Request) {
             num_images: 2,
           },
           {
-            text: `face like character from pixar movies, ohwx ${type} in business suit`,
+            text: `face like a character from pixar movies, ohwx ${type} in business suit, full color backdrops, disney, dreamworks`,
             // text: `pixar style animated linkedin profile picture of ohwx ${type}, face like characters from the Incredibles movies, wearing a business suit, professional headshots, full color backdrops, disney style, dreamworks style`,
             negative_prompt:
               'painting, grey background, extra fingers, mutated hands, poorly drawn hands, ((poorly drawn face,)) deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime, nude, NSFW',
