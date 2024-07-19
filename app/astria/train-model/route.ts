6b8ef691ee828d37c319fa0342df97668fb979d3
Brecht Horn <brecht.horn@mac.com>
2024-07-18 17:42:47 -0700

    updates prompts

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -142,17 +142,17 @@ export async function POST(request: Request) {
             text: `face like a character from the Toy Story movies, ohwx ${type} wearing a business suit,only one person, photo accurate hair color, photo accurate eyebrow color, professional photo, animated office background with skyscrapers outside window, disney style, dreamworks style`,
             // text: `pixar style animated portrait of ohwx ${type} wearing a business suit, face like characters from the Toy Story movies, only one person, photo accurate hair color, photo accurate eyebrow color, professional photo, animated office background with skyscrapers outside window, disney style, dreamworks style`,
             negative_prompt:
-              'painting, extra fingers, mutated hands, poorly drawn hands, ((poorly drawn face,)) deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime, nude, NSFW',
+              'painting, cowboy outfit, plaid shirt, jeans, sherrif oufit, extra fingers, mutated hands, poorly drawn hands, ((poorly drawn face,)) deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime, nude, NSFW',
             callback: promptWebhookWithParams,
-            num_images: 2,
+            num_images: 6,
           },
           {
-            text: `face like a character from the Toy Story movies, ohwx ${type} wearing a business suit, photo accurate hair color, photo accurate eyebrow color, animated park in the city with benches background, disney style, dreamworks style`,
+            text: `ohwx ${type} wearing a business suit, face like characters from the Toy Story movies, photo accurate hair color, photo accurate eyebrow color, animated park in with benches background, disney style, dreamworks style`,
             // text: `pixar style animated linkedin profile picture of ohwx ${type}, face like characters from the Incredibles movies, wearing a business suit, professional headshots, full color backdrops, disney style, dreamworks style`,
             negative_prompt:
-              'painting, grey background, gray background, extra fingers, mutated hands, poorly drawn hands, ((poorly drawn face,)) deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime, nude, NSFW',
+              'painting, cowboy outfit, plaid shirt, jeans, sherrif oufit, grey background, gray background, extra fingers, mutated hands, poorly drawn hands, ((poorly drawn face,)) deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime, nude, NSFW',
             callback: promptWebhookWithParams,
-            num_images: 2,
+            num_images: 6,
           },
         ],
       },
