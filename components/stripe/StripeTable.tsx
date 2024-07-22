558145ca41b6b7c741a7188d79c8c5fd376c106d
Brecht Horn <brecht.horn@mac.com>
2024-07-22 16:52:41 -0700

    updates domain and pricing

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -1,8 +1,12 @@
-'use client'
+'use client';
 import { User } from '@supabase/supabase-js';
 import React, { useEffect } from 'react';
 
-interface StripePricingTableProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+interface StripePricingTableProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
   'pricing-table-id': string;
   'publishable-key': string;
 }
@@ -17,33 +21,31 @@ declare global {
 
 type Props = {
   user: User;
-}
+};
 
 const StripePricingTable = ({ user }: Props) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "https://js.stripe.com/v3/pricing-table.js";
+    script.src = 'https://js.stripe.com/v3/pricing-table.js';
     script.async = true;
 
     document.body.appendChild(script);
 
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
   return (
     <div className='flex flex-1 flex-col w-full'>
       <stripe-pricing-table
-          pricing-table-id="prctbl_1PctqhRwyr1sGJYs2lgZj7d7"
-          publishable-key="pk_live_51PctK6Rwyr1sGJYslFlAbEtzczc5SsGBOtcC6ssayay2nrQVYuBxqnFDXCNg9oRjOolHDVJ0JO8KExMJLkSmDBMz00TiJNpL3c"
-          client-reference-id={user.id}
-          customer-email={user.email}
-      >
-      </stripe-pricing-table>
+        pricing-table-id='prctbl_1PfW0qRwyr1sGJYsS7eu2m3c'
+        publishable-key='pk_live_51PctK6Rwyr1sGJYslFlAbEtzczc5SsGBOtcC6ssayay2nrQVYuBxqnFDXCNg9oRjOolHDVJ0JO8KExMJLkSmDBMz00TiJNpL3c'
+        client-reference-id={user.id}
+        customer-email={user.email}
+      ></stripe-pricing-table>
     </div>
   );
-}
-
+};
 
 export default StripePricingTable;
